490006ba2869947fc153803567d465eb5889d591
maros <popovma1@fit.cvut.cz>
2022-08-11 16:27:28 +0200

    FEAT: create gyroscope demo in fragment

--- SpaceX/app/src/main/java/cz/cvut/popovma1/spacex/feature/rocketlaunch/system/RocketLaunchFragment.kt
+++ SpaceX/app/src/main/java/cz/cvut/popovma1/spacex/feature/rocketlaunch/system/RocketLaunchFragment.kt
@@ -1,5 +1,10 @@
 package cz.cvut.popovma1.spacex.feature.rocketlaunch.system
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,9 +16,16 @@ import androidx.navigation.fragment.navArgs
 import cz.cvut.popovma1.spacex.ui.theme.SpaceXTheme
 import quanti.com.kotlinlog.Log
 
-class RocketLaunchFragment : Fragment() {
+class RocketLaunchFragment : Fragment(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
 
     private val args: RocketLaunchFragmentArgs by navArgs()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setUpSensor()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,5 +46,37 @@ class RocketLaunchFragment : Fragment() {
     private fun navigateBack() {
         Log.d("navigateBack() pressed")
         findNavController().popBackStack()
+    }
+
+    // todo move away from fragment
+    private fun setUpSensor() {
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)?.also {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_NORMAL,
+            )
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if(event?.sensor?.type == Sensor.TYPE_GYROSCOPE) {
+            val xRotation = event.values[0]
+//            Log.d("X axis rotation: $xRotation")
+            if(xRotation > 3) {
+                Log.d("X axis rotation: Launch !")
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // do nothing
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(this)
     }
 }
