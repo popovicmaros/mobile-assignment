e97ed8b87d783b99f4c503e3f4c3668a11fe6d5e
maros <popovma1@fit.cvut.cz>
2022-08-23 10:01:51 +0200

    FEAT: use ComponentActivity

--- SpaceX/app/src/main/java/cz/cvut/popovma1/spacex/MainActivity.kt
+++ SpaceX/app/src/main/java/cz/cvut/popovma1/spacex/MainActivity.kt
@@ -1,15 +1,14 @@
 package cz.cvut.popovma1.spacex
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import cz.cvut.popovma1.spacex.ui.navigation.Navigation
 import cz.cvut.popovma1.spacex.ui.theme.SpaceXTheme
 import cz.cvut.popovma1.spacex.util.LoggingUtil
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Theme_SpaceX)
         super.onCreate(savedInstanceState)
         setContent {
             SpaceXTheme {
