cf34b203ded0e9baeb80de7257876ab127767067
maros <popovma1@fit.cvut.cz>
2022-08-25 09:11:33 +0200

    VERSION: 1.3.0

--- SpaceX/app/build.gradle.kts
+++ SpaceX/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 21
         targetSdk = 32
         versionCode = 1
-        versionName = "1.2.0"
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
